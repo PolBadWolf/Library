5d15d15de244b4a10da7b12fa95ff0e8988fb641
Doc <gm.gameoff@gmail.com>
2020-11-24 18:48:15 +0500

    add swing utils & swing components mod

---
+++ swing_componens_modifed/src/main/java/ru/yandex/fixcolor/library/swing/components/modifed/MPanel.java
@@ -0,0 +1,34 @@
+package ru.yandex.fixcolor.library.swing.components.modifed;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+public class MPanel extends MPanelPrintableCap {
+    public BufferedImage image = null;
+    public double scale_img = 1.0;
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (image == null) return;
+        Graphics2D g2 = (Graphics2D) g;
+        AffineTransform oldAt = g2.getTransform();
+        try {
+            double scale_imgI = 1 / scale_img;
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            g2.scale(scale_imgI, scale_imgI);
+            g2.drawRenderedImage(image, null);
+        } finally {
+            g2.setTransform(oldAt);
+        }
+    }
+
+    @Override
+    protected void paintChildren(Graphics g) {
+        super.paintChildren(g);
+    }
+
+}
